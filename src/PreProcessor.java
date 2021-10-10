5f1cfefd32f1ee4d052ae04f561ac3581938b382
abolfazl <akabolfazl96@gmail.com>
2021-10-10 14:06:11 +0330

    add preprocessor class and pattern class

---
+++ src/PreProcessor.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PreProcessor {
+
+}
--- src/Scanner.java
+++ src/Scanner.java
@@ -1,2 +1,12 @@
+import java.util.regex.Pattern;
+
 public class Scanner {
+}
+
+class Patterns {
+    Pattern _while = Pattern.compile("");
+    Pattern _if = Pattern.compile("");
+    Pattern _for = Pattern.compile("");
+    Pattern _else = Pattern.compile("");
+    Pattern _string = Pattern.compile("");
 }

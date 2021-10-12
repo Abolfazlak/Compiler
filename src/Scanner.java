ce7388506778fed25f866cc4fac3b5c16f481aac
maryam <maryamnik010@gmail.nik>
2021-10-12 12:19:16 +0330

    add some patterns

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,2 +1,16 @@
-public class Scanner {
-}
+import java.util.regex.Pattern;
+public class Scanner {}
+    class Patterns {
+        Pattern _while = Pattern.compile("(while)(\\s*)(\\()(.*)(\\))");
+        Pattern _if = Pattern.compile("(if)(\\s*)(\\()(.*)(\\))");
+        Pattern _for = Pattern.compile("(for)(\\s*)(\\()(.*)(\\))");
+        Pattern _else = Pattern.compile("(else)");
+        Pattern _id = Pattern.compile("([a-z]|[A-Z]).([a-z]|[A-Z]|[0-9]|\\_)*");
+        Pattern _int10 = Pattern.compile("([0-9])*");
+        Pattern _int16 = Pattern.compile("0(x|X).([A-F]|[a-f]|[0-9])*");
+        Pattern _double = Pattern.compile("([0-9])+(\\.)([0-9])*");
+        Pattern _SN = Pattern.compile("([0-9]+)(\\.)([0-9]*)(e|E)(\\+|\\-?)([0-9])");
+        Pattern _operators = Pattern.compile("(\\+(\\=?))|(\\-(\\=?))|(\\*(\\=?))|(\\/(\\=?))|(\\%)|(\\<(\\=?))|(\\>(\\=?))|(\\=(\\=?))|(\\!(\\=?))|(\\&\\&)|(\\|\\|)|(\\;)|(\\,)|(\\.)|(\\[)|(\\])|(\\()|(\\))|(\\{)|(\\})");
+        Pattern _comment = Pattern.compile("((\\/\\/)(.*))|((\\/\\*)((.*)(\\n?))*(\\*\\/))");
+
+    }
